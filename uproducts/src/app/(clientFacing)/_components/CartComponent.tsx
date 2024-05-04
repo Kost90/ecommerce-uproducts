6113f563ea7086d222ba42d572bfc6665c45282a
kost90 <kostdonc90@gmail.com>
2024-05-04 11:24:17 +0100

    added sheet component to the CartComponent after clicking on basket button in navbar

--- uproducts/src/app/(clientFacing)/_components/CartComponent.tsx
+++ uproducts/src/app/(clientFacing)/_components/CartComponent.tsx
@@ -2,8 +2,19 @@
 import { useRef } from "react";
 import { useAppSelector, useAppStore } from "@/lib/hooks";
 import { NavLink } from "@/components/Nav";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { ShoppingBasket } from "lucide-react";
 import { selectOrders } from "@/lib/features/orders/ordersSlice";
+import { Button } from "@/components/ui/button";
 
 function CartComponent() {
   // TODO:Import state from redux store - about count of orders.
@@ -16,15 +27,42 @@ function CartComponent() {
   const orders = useAppSelector(selectOrders);
 
   return (
-    <div className="flex flex-row justify-center items-center group">
-      <NavLink href="/cart">
-        <ShoppingBasket />
-      </NavLink>
-      <span className="font-medium text-slate-500 group-hover:text-primary">
-        {orders.length}
-      </span>
-    </div>
+    <Sheet>
+      <SheetTrigger>
+        <div className="flex flex-row justify-center items-center group">
+        <ShoppingBasket className="text-slate-500 group-hover:text-primary"/>
+          <span className="font-medium text-slate-500 group-hover:text-primary">
+            {orders.length}
+          </span>
+        </div>
+      </SheetTrigger>
+      <SheetDisplya/>
+    </Sheet>
   );
+}
+
+// ! Передаю сюда пропсами данные из редакс стора о заказе или заказах.
+function SheetDisplya(){
+
+  return(
+    <SheetContent>
+    <SheetHeader>
+      <SheetTitle>Are you absolutely sure?</SheetTitle>
+      <SheetDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </SheetDescription>
+    </SheetHeader>
+
+    {/* Сюда добавляю тело щита - все о товарах которые в корзине */}
+
+    <SheetFooter>
+      <SheetClose asChild>
+      <NavLink href="/cart"><Button>Checkout</Button></NavLink>
+      </SheetClose>
+    </SheetFooter>
+  </SheetContent>
+  )
 }
 
 export default CartComponent;

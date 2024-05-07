33098328bf57a089c9716b941a7866abf539ae2f
kost90 <kostdonc90@gmail.com>
2024-05-07 15:19:22 +0100

    created cardComponent for clientFacing side, added grid section to the home page

--- uproducts/src/lib/utils.ts
+++ uproducts/src/lib/utils.ts
@@ -1,6 +1,24 @@
+import { getProductsUrl } from "@/app/admin/_actions/ProductsActions";
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+// Function for fetching data from server and DB
+export async function getData() {
+  const res = await fetch("http://localhost:3001/products");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const products = await res.json();
+
+  for (let product of products) {
+    product.imagePath = await getProductsUrl(product.imageKey);
+  }
+
+  return products;
 }

df632ef59d62d9435aea814574715437c0310a2f
Kostiantyn Dontsov <kostdonc90@gmail.com>
2025-02-05 20:14:26 +0000

    - edited eslint config - edited Form component

--- uproducts/src/components/form/Form.tsx
+++ uproducts/src/components/form/Form.tsx
@@ -1,14 +1,41 @@
+// import React from 'react';
+
+// interface IFormProps<T> {
+//   children: React.ReactNode;
+//   action: (payload: FormData) => T;
+//   className: string;
+// }
+
+// function Form<T extends string | ((formData: FormData) => void | Promise<void>) | undefined>({
+//   children,
+//   action,
+//   className,
+// }: IFormProps<T>) {
+//   return (
+//     <form action={action} className={className}>
+//       {children}
+//     </form>
+//   );
+// }
+
+// export default Form;
 import React from 'react';
 
-interface IFormProps<T> {
+interface IFormProps {
   children: React.ReactNode;
-  action: (payload: FormData) => T;
-  className: string;
+  action: (payload: FormData) => void | Promise<void>;
+  className?: string;
 }
 
-function Form<T>({ children, action, className }: IFormProps<T>) {
+function Form({ children, action, className }: IFormProps) {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    action(formData);
+  };
+
   return (
-    <form action={action} className={className}>
+    <form onSubmit={handleSubmit} className={className}>
       {children}
     </form>
   );

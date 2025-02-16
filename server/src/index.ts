36c0b1d702f44249ae259c3b1d0679a6acd31184
Kostiantyn Dontsov <kostdonc90@gmail.com>
2025-02-16 15:36:12 +0000

    - added one more method to the cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -21,7 +21,7 @@ app.use(
   cors({
     origin: [config.forntUrl, 'http://localhost:3000'],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   }),
 );

c1f0a74ec5869d4cbb4af2ba24d608556cd1f928
Kostiantyn Dontsov <kostdonc90@gmail.com>
2025-02-19 12:59:10 +0000

    - added logic for create and update user address on backend (routes, validators, services, repo) - created migration for added  userId field to the Address table

--- server/src/routes/UserRoutes.ts
+++ server/src/routes/UserRoutes.ts
@@ -4,6 +4,7 @@ import RequestMiddleware from '../midlewares/requestMidleware';
 import { PrismaClient } from '@prisma/client';
 import InjectionService from '../services/injectionServiceFactory';
 import UserController from '../controllers/UserController';
+import UserActionValidators from '../validators/user';
 
 const prisma = new PrismaClient();
 const injectionService = new InjectionService(prisma);
@@ -17,4 +18,19 @@ router.get(
   (req, res, next) => authenticationMiddleware.verifyAccesse(req, res, next),
   RequestMiddleware.validateRequest,
   (req: Request, res: Response, next: NextFunction) => userController.getUser(req, res, next),
+);
+router.post(
+  '/:userId/address',
+  AuthenticationMiddleware.verifyApiKey,
+  UserActionValidators.postAddress(),
+  RequestMiddleware.validateRequest,
+  (req: Request, res: Response, next: NextFunction) => userController.postAddress(req, res, next),
+);
+
+router.patch(
+  '/:userId/address/update',
+  AuthenticationMiddleware.verifyApiKey,
+  UserActionValidators.postAddress(),
+  RequestMiddleware.validateRequest,
+  (req: Request, res: Response, next: NextFunction) => userController.updateUserAddress(req, res, next),
 );
